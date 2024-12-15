79fa818a300e33a8347398ce4d2f60be366d135c
rdb <git@rdb.name>
2024-12-15 15:42:34 +0100

    Automatically remove duplicate reminders

--- lib/reminders.py
+++ lib/reminders.py
@@ -22,6 +22,12 @@ class Reminder:
         elif self.repeat_interval == 'year':
             return timedelta(years=1)
 
+    def __eq__(self, other):
+        return (self.time == other.time and
+                self.text == other.text and
+                self.repeat == other.repeat and
+                self.repeat_interval == other.repeat_interval)
+
     def __str__(self):
         if self.repeat:
             return f'{self.time}: {self.text} (Repeats every {self.repeat_interval})'
@@ -29,14 +35,16 @@ class Reminder:
     
     def __repr__(self):
         return str(self)
-    
+
+
 class Reminders:
     def __init__(self):
         self.reminders = []
     
     def add_reminder(self, reminder):
-        self.reminders.append(reminder)
-        self.save()
+        if reminder not in self.reminders:
+            self.reminders.append(reminder)
+            self.save()
 
     def get_reminders(self):
         return self.reminders
@@ -72,20 +80,29 @@ class Reminders:
         if not os.path.isfile('reminders.json'):
             # Create the file
             self.save()
+
+        reminders = []
+        num_dupes = 0
         with open('reminders.json', 'r') as f:
-            reminders_json = f.read()
-        reminders_dict = json.loads(reminders_json)
-        # Need to parse the time back into a datetime
-        self.reminders = [Reminder(datetime.fromisoformat(reminder['time']), reminder['text'], reminder['repeat'], reminder['repeat_interval']) for reminder in reminders_dict]
-        return self.reminders
-        
+            for reminder_dict in json.load(f):
+                reminder = Reminder(datetime.fromisoformat(reminder_dict['time']), reminder_dict['text'], reminder_dict['repeat'], reminder_dict['repeat_interval'])
+                if reminder not in reminders:
+                    reminders.append(reminder)
+                else:
+                    num_dupes += 1
+
+        self.reminders = reminders
+        if num_dupes > 0:
+            print(f'Removed {num_dupes} duplicate reminders')
+            self.save()
+        return reminders
+
     def __str__(self):
         # Order the reminders by time, starting with the next one
         self.reminders.sort(key=lambda x: x.time)
         reminders_string = '\n'.join([str(reminder) for reminder in self.reminders])
         return reminders_string
-    
+
     def todays_reminders(self):
         current_date_utc = datetime.now(timezone.utc).date()
         return [reminder for reminder in self.reminders if reminder.time.date() == current_date_utc]
-    
